dcda6b9e90a4326ad735ad250609aca68b75a765
gengjozsef <gengjozsef86@gmail.com>
2020-06-22 17:18:18 +0200

    change inheritDoc annotation

--- src/FondOfSpryker/Zed/ProductListBrandConnector/Business/ProductListBrandConnectorFacade.php
+++ src/FondOfSpryker/Zed/ProductListBrandConnector/Business/ProductListBrandConnectorFacade.php
@@ -11,7 +11,7 @@ use Spryker\Zed\Kernel\Business\AbstractFacade;
 class ProductListBrandConnectorFacade extends AbstractFacade implements ProductListBrandConnectorFacadeInterface
 {
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      *
      * @api
      *
@@ -27,7 +27,7 @@ class ProductListBrandConnectorFacade extends AbstractFacade implements ProductL
     }
 
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      *
      * @api
      *
